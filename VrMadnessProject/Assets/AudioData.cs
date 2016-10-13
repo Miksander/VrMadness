2b14bb1990fdf64993dc97def30b1f2ba6ad9fe2
Tuukka Muukkonen <tuukka.muukkonen@opisto.hkro>
2016-10-13 16:57:00 +0300

    Tuukan scene, reactive box, AudioData scrit that captures data from mic

---
+++ VrMadnessProject/Assets/AudioData.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioData : MonoBehaviour {
+
+	public int sampleRate = 2048;
+	public AudioSource source;
+
+	float[] samples;
+	AudioClip clip;
+
+	void Start () {
+		samples = new float[sampleRate]; 
+		clip = Microphone.Start(Microphone.devices[0], true, 1, sampleRate);
+		source.clip = clip;
+		source.Play();
+	}
+
+	void Update () {
+		//clip = Microphone.Start(Microphone.devices[0], true, 1, sampleRate);
+		//source.clip = clip;
+		//source.Play();
+		source.GetSpectrumData(samples, 0, FFTWindow.Rectangular);
+	}
+
+	public float[] GetMicData () {
+		return samples;
+	}
+}
---
+++ VrMadnessProject/Assets/ReactiveBox.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactiveBox : MonoBehaviour {
+
+	public float toBounce;
+	public float force;
+	public AudioData _aData;
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		var _dataSum = 0f;
+		var _micData = _aData.GetMicData();
+		for (int i = 0; i < _micData.Length; i++) {
+			_dataSum += _micData[i];
+		}
+		
+		//transform.localScale = new Vector3(1, _dataSum, 1);
+		if (_dataSum > toBounce) {
+			rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+		}
+	}
+}
