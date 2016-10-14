42419149080ccfd83b87d5cbb8ea396fb1f300f2
unknown <mikko.aaltonen@peli25-pc.opisto.hkro>
2016-10-14 13:15:38 +0300

    testing

--- VrMadnessProject/Assets/ParticleController.cs
+++ VrMadnessProject/Assets/ParticleController.cs
@@ -5,9 +5,16 @@ public class ParticleController : MonoBehaviour {
 
     public GameObject _ps;
     public float _waitTime;
+    ParticleSystem particleSystem;
+
+    void Start ()
+    {
+        particleSystem = _ps.GetComponent<ParticleSystem>();
+    }
 
     public IEnumerator Emit ()
     {
+       // particleSystem.Emit(1000);
         _ps.SetActive(true);
         yield return new WaitForSeconds(_waitTime);
         _ps.SetActive(false);
