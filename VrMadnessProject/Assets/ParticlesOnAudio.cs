dd333b7a04b6381c03cbf0cd4f95990390e137a3
Miksander <mikko1aaltonen@gmail.com>
2016-10-20 09:56:47 +0300

    new effect on camera, audio reactive

---
+++ VrMadnessProject/Assets/ParticlesOnAudio.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+
+public class ParticlesOnAudio : MonoBehaviour
+{
+    
+    public int _band;
+    public float _minEmissionRate, maxEmissionRate;
+    ParticleSystem _par;
+
+    // Use this for initialization
+    void Start()
+    {
+        _par = GetComponent<ParticleSystem>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var emission = _par.emission;
+        var rate = emission.rate;
+
+        rate.constantMax = (AudioPeer._audioBandBuffer[_band] * (maxEmissionRate - _minEmissionRate)) + _minEmissionRate; 
+        emission.rate = rate;
+        // (AudioPeer._audioBandBuffer[_band] * (maxEmissionRate - _minEmissionRate)) + _minEmissionRate;
+    }
+}
