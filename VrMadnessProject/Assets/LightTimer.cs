a047e31ad2ebc8c4ac6f904c84e26f744303f97b
Miksander <mikko1aaltonen@gmail.com>
2016-10-26 12:17:45 +0300

    light timer script added to lights, animation for TranceCube done

---
+++ VrMadnessProject/Assets/LightTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTimer : MonoBehaviour {
+
+    public float startDelay = 1f;
+    public float playTime = 2f;
+
+    Light _light;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        _light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time > startDelay + playTime)
+        {
+
+            _light.enabled=false;
+        }
+        else if (Time.time > startDelay)
+        {
+
+            _light.enabled=true;
+        }
+    }
+}
